4b37a42b7b9a83820d8e9113546e02260ba47f62
trident6 <philip.johnson.it@gmail.com>
2020-09-23 22:37:33 -0400

    work in progress

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
 const currencies = [
-  'EUR',
-  'CAD',
-  'HKD',
-  'ISK',
-  'PHP',
-  'DKK',
-  'HUF',
-  'CZK',
-  'AUD',
-  'RON',
-  'SEK',
-  'IDR',
-  'INR',
-  'BRL',
-  'RUB',
-  'HRK',
-  'JPY',
-  'THB',
-  'CHF',
-  'SGD',
-  'PLN',
-  'BGN',
-  'TRY',
-  'CNY',
-  'NOK',
-  'NZD',
-  'ZAR',
-  'USD',
-  'MXN',
-  'ILS',
-  'GBP',
-  'KRW',
-  'MYR'
+    'EUR',
+    'CAD',
+    'HKD',
+    'ISK',
+    'PHP',
+    'DKK',
+    'HUF',
+    'CZK',
+    'AUD',
+    'RON',
+    'SEK',
+    'IDR',
+    'INR',
+    'BRL',
+    'RUB',
+    'HRK',
+    'JPY',
+    'THB',
+    'CHF',
+    'SGD',
+    'PLN',
+    'BGN',
+    'TRY',
+    'CNY',
+    'NOK',
+    'NZD',
+    'ZAR',
+    'USD',
+    'MXN',
+    'ILS',
+    'GBP',
+    'KRW',
+    'MYR'
 ]
+const userUrl = 'https://api.exchangeratesapi.io/latest?base='
 
+// const targetEl = document.querySelector('#curSelect')
+
+
+for (let currency of currencies) {
+    let targetEl = document.querySelector('.from')
+    let option = document.createElement('option')
+    option.innerText = currency
+    option.value = currency
+    targetEl.appendChild(option)
+        // console.log(option, 'from')
+
+}
+
+for (let currency of currencies) {
+    let targetEl = document.querySelector('.to')
+    let option = document.createElement('option')
+    option.innerText = currency
+    option.value = currency
+    targetEl.appendChild(option)
+        // console.log(option, 'to')
+
+}
